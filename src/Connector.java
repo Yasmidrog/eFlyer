61018f4fb3c29d0f7f1753f501d360b86e698b2c
yasmidrog <Password!slavik!>
2015-10-08 23:12:37 +0300

    TestBuild

--- src/Connector.java
+++ src/Connector.java
@@ -11,13 +11,6 @@ import java.util.Enumeration;
 
 public class Connector implements SerialPortEventListener {
     SerialPort serialPort;
-    /** The port we're normally going to use. */
-    private static final String PORT_NAMES[] = {
-            "/dev/tty.usbserial-A9007UX1", // Mac OS X
-            "/dev/ttyACM0", // Raspberry Pi
-            "/dev/ttyUSB0", // Linux
-            "COM3", // Windows
-    };
     /**
      * A BufferedReader which will be fed by a InputStreamReader
      * converting the bytes into characters
@@ -34,20 +27,14 @@ public class Connector implements SerialPortEventListener {
     public void initialize() {
         // the next line is for Raspberry Pi and
         // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM1");
 
         CommPortIdentifier portId = null;
-        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+        try {
+            portId = CommPortIdentifier.getPortIdentifier("/dev/ttyACM1");
 
-        //First, Find an instance of serial port as set in PORT_NAMES.
-        while (portEnum.hasMoreElements()) {
-            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-            for (String portName : PORT_NAMES) {
-                if (currPortId.getName().equals(portName)) {
-                    portId = currPortId;
-                    break;
-                }
-            }
+        }catch (Exception e){
+           e.printStackTrace();
         }
         if (portId == null) {
             System.out.println("Could not find COM port.");
