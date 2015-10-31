81690f2e34cd3ea13db51f8098643e1142779df6
yasmidrog <yasmidrog@gmail.com>
2015-10-31 20:33:26 +0300

    Add GPS

--- src/com/fruitmix/raspiconnect/GPSConnector.java
+++ src/com/fruitmix/raspiconnect/GPSConnector.java
@@ -16,12 +16,22 @@ public class GPSConnector extends Connector {
                 String gps= input.readLine();
                 if(gps.contains("$GPGLL")){
                     String[] need = gps.split(",");
-                    double n = Double.parseDouble(need[1])/100, e = Double.parseDouble(need[3])/100, a = Double.parseDouble(need[5])/1000;
-                    String o = n + "N "+ e+"E"+ "\n" + "Altitude:"+a;
-                    System.out.println(o);
+                    double n = Double.parseDouble(need[1])/100, e = Double.parseDouble(need[3])/100,
+                            a = Double.parseDouble(need[5])/1000;
+                    String.format("%.2f",1.12);
+                    String o = round(n,2) + "N "+ round(e,2)+"E"+ "\n" + "Altitude:"+round(a,2);
+                    System.out.print(o);
                 }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static float round(double number, int scale) {
+        int pow = 10;
+        for (int i = 1; i < scale; i++)
+            pow *= 10;
+        double tmp = number * pow;
+        return (float) (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) / pow;
     }
 }
